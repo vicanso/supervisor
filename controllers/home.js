e880c7a0743ed7e37fc8c56a1070b2c997c4ff57
vicanso <vicansocanbico@gmail.com>
2015-06-05 21:48:19 +0800

    commit

--- controllers/home.js
+++ controllers/home.js
@@ -1,6 +1,7 @@
 'use strict';
 var debug = require('../helpers/debug');
 module.exports = function *(){
+  this.set('Cache-Control', 'public, max-age=600');
   this.state.viewData = {
     page : 'home'
   };
