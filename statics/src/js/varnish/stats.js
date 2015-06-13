caaa9854d4bc4720be217eef1491e8d8cb7a90dc
vicanso <vicansocanbico@gmail.com>
2015-06-13 21:44:48 +0800

    commit

--- statics/src/js/varnish/stats.js
+++ statics/src/js/varnish/stats.js
@@ -3,22 +3,60 @@
 var fn = function($scope, $http, $element, $q, $timeout, debug, vs){
   debug = debug('jt.varnishStatsPage');
   var varnishList = JT_GLOBAL.varnishList;
-  var interval = 60 * 1000;
   var statsList = {};
 
 
   var self = this;
+
+  // 指标统计相关信息
+  self.statsConfig = {};
   self.statsList = null;
   self.init = false;
 
-  self.quotaList = vs.quotaList();
+  self.quotaList = getQuotaList();
 
+  self.start = start;
 
-  // startMonitor(varnishList);
+  $scope.$watch('varnishStats.statsConfig.interval', function(){
+    self.statsConfig.status = '';
+  });
   return self;
 
+  function start(){
+    var statsConfig = self.statsConfig;
+    if(!statsConfig.interval){
+      statsConfig.error = '时间间隔不能为空';
+      statsConfig.status = 'error';
+      return;
+    }
+    startMonitor(varnishList);
+    self.init = true;
+  }
 
+  function getQuotaList(){
+    var list = vs.quotaList();
+    var defaultCheckedList = 'uptime sess_conn sess_drop sess_fail client_req cache_hit cache_hitpass cache_miss backend_conn backend_unhealthy backend_busy backend_fail backend_reuse backend_toolate backend_recycle backend_retry fetch_length fetch_chunked fetch_failed n_lru_nuked n_lru_moved backend_req';
+    var result = [];
+    angular.forEach(list, function(name){
+      var checked = false;
+      if(defaultCheckedList.indexOf(name) !== -1){
+        checked = true;
+      }
+      result.push({
+        name : name,
+        checked : checked
+      })
+    });
+    return result;
+  }
+
+  /**
+   * [startMonitor description]
+   * @param  {[type]} list [description]
+   * @return {[type]}      [description]
+   */
   function startMonitor(list){
+    var interval = self.statsConfig.interval * 1000;
     vs.statsList(list).then(function(arr){
       angular.forEach(arr, function(res, i){
         var data = res.data;
